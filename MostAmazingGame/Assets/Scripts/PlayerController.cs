44dac1d59311f262a7c21d2826d8b12ad94fa603
LAPTOP-76TSKG7G\tanke <tankevin@usc.edu>
2018-09-13 11:22:52 -0700

    current code

---
+++ MostAmazingGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed = .25f;
+    private Rigidbody2D rb;
+    private Object player;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+        movement *= moveSpeed;
+        this.transform.position += movement;
+    }
+}
