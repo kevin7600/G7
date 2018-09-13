c93634a8986eb9dd176ffc51c27928306c3484f2
LAPTOP-76TSKG7G\tanke <tankevin@usc.edu>
2018-09-13 15:37:22 -0700

    player movement and shooting (and map kinda)

---
+++ MostAmazingGame/Assets/Scripts/ShootButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public bool Pressed;
+    public bool fired;
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+}
